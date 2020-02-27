fdfc09c7e823334e8aa9d3d3e4db1afaabb8b195
Doug <doug@withseismic.com>
2020-02-27 14:13:25 +0100

    easily

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,46 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<v-card  max-width="350px">
+<button @click="toggleColor" >Button!</button>
+<h3>Product Title</h3>
+<p :class="{ cardIsActive: active}">{{sayHello}} is a product description {{$store.state.counter}} for our simple store product!</p>
+
+<v-img src=""></v-img>
+
+</v-card>
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+  },
+  data: () => {
+    return {
+      active: false,
+      message: "hello!",
+      name: "joe"
+    }  
+  }, 
+  methods: {
+    toggleColor () {
+      this.active = !this.active
+    },
+  },
+  computed: {
+    sayHello() {
+      return this.$store.state.counter + 1
+    }
   }
 }
 </script>
+
+<style lang="scss">
+
+  .cardIsActive { background-color: red}
+
+ </style>
